6fe0a29c6dee8065ef69d34f7c0acab58f956e3d
Mehdi <elkouaratimehdi@gmail.com>
2026-03-12 20:22:44 +0100

    begining of the fct multiclass

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -31,3 +31,28 @@ class LogisticRegression:
     def predict(self,X):
         y_pred=self.sigmoid(X@self.w+self.b)
         return (y_pred>=0.5).astype(int)
+
+class LogisticRegressionL2(LogisticRegression):
+    def __init__(self, learning_rate=0.01, n_iterations=1000, lambda_=0.1):
+        super().__init__(learning_rate, n_iterations)
+        self.lambda_=lambda_
+
+    def logisticloss(self,y,y_pred):
+        y_pred = np.clip(y_pred, 1e-9, 1 - 1e-9) #Tto avoid prob 0 or 1
+        return -np.mean(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred)) + self.lambda_ * np.sum(self.w**2)
+
+    def fit(self, X,y):
+        self.w=np.zeros(X.shape[1])
+        self.b=0
+        self.losses=[]
+        #Gradient descent to find the wieghts w and b
+        for i in range(self.n_iterations):
+            y_pred=self.sigmoid(X@self.w+self.b)
+            current_loss=self.logisticloss(y,y_pred)
+            self.losses.append(current_loss)
+            grad_w=1/X.shape[0]*(X.T@(y_pred-y)) + 2*self.lambda_ * self.w
+            grad_b=1/X.shape[0]*np.sum(y_pred-y)
+            self.w-=self.learning_rate*grad_w
+            self.b-=self.learning_rate*grad_b
+
+class LogisticRegressionmulticlass:
