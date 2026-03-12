fad31c587686cfec996d3baca44862b38e91dcad
Mehdi <elkouaratimehdi@gmail.com>
2026-03-12 15:55:47 +0100

    fix bug

--- linear_regression.py
+++ linear_regression.py
@@ -6,6 +6,9 @@ class LinearRegression:
     
     def fit(self,X,Y):
         self.w=np.linalg.inv(X.T@X)@X.T@Y
-        
+    
+    def fit_ridge(self,X,Y,alpha):
+        self.w=np.linalg.inv(X.T@X+alpha*(X.shape[0])*np.eye(X.shape[1]))@X.T@Y
+    
     def predict(self,X):
         return X@self.w
