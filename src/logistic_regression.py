31b03195d1b457d318229cccc3a62abb7ca9f6da
Mehdi <elkouaratimehdi@gmail.com>
2026-03-19 16:41:01 +0100

    multiclass implimentation done and test with sklearn

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, learning_rate=0.01, n_iterations=1000):
+    def __init__(self, learning_rate=0.01, n_iterations=100):
         self.w=None
         self.b=None
         self.learning_rate=learning_rate
@@ -35,7 +35,7 @@ class LogisticRegression:
         return (y_pred>=0.5).astype(int)
 
 class LogisticRegressionL2(LogisticRegression):
-    def __init__(self, learning_rate=0.01, n_iterations=1000, lambda_=0.1):
+    def __init__(self, learning_rate=0.01, n_iterations=100, lambda_=0.001):
         super().__init__(learning_rate, n_iterations)
         self.lambda_=lambda_
 
@@ -59,18 +59,41 @@ class LogisticRegressionL2(LogisticRegression):
             self.b-=self.learning_rate*grad_b
 
 class LogisticRegressionmulticlass:
-    def __init__(self,learnin_rate=0.01,n_iterations=1000):
+    def __init__(self,learning_rate=0.01,n_iterations=100):
         self.w=None
         self.b=None
-        self.learning_rate=self.learning_rate
+        self.learning_rate=learning_rate
         self.n_iterations=n_iterations
 
     @staticmethod
     def softmax(z):
-        return np.exp(z)/np.sum(np.exp(z))
+        exp_z = np.exp(z)
+        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
     
+    @staticmethod
     def crossentropyloss(y,y_pred):
-        return -np.sum(y*np.log(y_pred))
+        y_pred = np.clip(y_pred, 1e-9, 1 - 1e-9)
+        return -np.mean(np.sum(y*np.log(y_pred), axis=1))
 
     def fit(self,X,y):
-        
+        self.w=np.zeros((X.shape[1],len(np.unique(y))))
+        self.b=np.zeros(len(np.unique(y)))
+        self.losses=[]
+        Y_one_hot = np.zeros((X.shape[0], len(np.unique(y)))) #to convert the labels to one hot encoding (matrix of 0 and 1)
+        Y_one_hot[np.arange(X.shape[0]), y] = 1
+        for i in range(self.n_iterations):
+            z=X@self.w +self.b
+            y_pred=self.softmax(z)
+
+            currentloss=self.crossentropyloss(Y_one_hot,y_pred)
+            self.losses.append(currentloss)
+            grad_w=(1/X.shape[0]) * (X.T)@(y_pred - Y_one_hot)
+            grad_b=(1/X.shape[0])* np.sum(y_pred-Y_one_hot,axis=0)
+            self.w=self.w - self.learning_rate*grad_w
+            self.b=self.b -self.learning_rate*grad_b
+    
+    def predict(self, X):
+        z=X@self.w +self.b
+        proba=self.softmax(z)
+        prediction=np.argmax(proba, axis=1)
+        return prediction
